13d815092efc4357896ec66d1dd44414f6f47faf
Tarana-naznin <tarananaznin21@gmail.com>
2025-03-22 11:12:23 +0100

    push repo

---
+++ src/main/java/com/example/low_code_demo/LowCodeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.low_code_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LowCodeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LowCodeDemoApplication.class, args);
+	}
+
+}
